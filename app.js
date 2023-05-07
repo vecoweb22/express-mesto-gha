18b2a404cbf71b92967df0489fae58efe2819e4e
vecoweb <vecoweb22@yandex.ru>
2023-05-08 01:30:06 +0300

    Fix app.js

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const routes = require('./routes/router');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb')
+  .then(() => {
+    console.log('БД подключена');
+  })
+  .catch(() => {
+    console.log('Не удалось подключиться к БД');
+  });
+
 app.use(helmet());
 app.disable('x-powered-by');
 app.use(express.json());
@@ -20,15 +29,6 @@ app.use((req, res, next) => {
 });
 
 app.use(routes);
-
-mongoose
-  .connect('mongodb://127.0.0.1:27017/mestodb')
-  .then(() => {
-    console.log('БД подключена');
-  })
-  .catch(() => {
-    console.log('Не удалось подключиться к БД');
-  });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
